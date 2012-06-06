994d085c97a909e4c6922f77f5069aaf9f55e6af
Michael Lovci <michaeltlovci@gmail.com>
2012-06-06 16:10:35 -0700

    split peakfinder call_peaks into two functions for wiggle-track functionality

--- parse_SE_sample.py
+++ parse_SE_sample.py
@@ -1,15 +1,5 @@
 import sys
-f = open(sys.argv[1])
-f.readline() #header
-for line in f.readlines():
-    event, misoMean, ciLow, ciHigh, isoforms, counts, acounts = line.strip().split("\t")
-    
-    inclusion = -1
-    exclusion = -1
-    
+from misopy.parse_csv import csv2dictlist_raw as cv
 
-    if not "," in misoMean:
-        inclusion = float(misoMean)
-        exclusion = 1-float(misoMean)
-    print "\t".join(map(str, [event, inclusion, exclusion]))
 
+data,h = cv(sys.arv[1])
