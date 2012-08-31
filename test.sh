ea5efa839f8ca7ca0d52d63c1ce147ea96ac768e
Gabriel Pratt <gpratt@ucsd.edu>
2012-08-30 21:49:38 -0700

    made fitting errors not kill program

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@
 
 #clipper -b clipper/test/allup_test.bam -s hg19
 #clipper -b /nas3/lovci/projects/FOX2/FOX2_mouse_brain/CLIP/analysis_gsnap/FOX2Brain.all.bam -s mm9
-clipper -b /nas3/lovci/projects/IMP2/CLIP/analysis_gsnap/IMP2_Hues6_4A_2010.tq3.norep_gsnaphg19.srt.bam -s hg19 
+clipper -b IMP2_Hues6_4A_2010.tq3.norep_gsnaphg19.srt.bam -s hg19 
 
 
