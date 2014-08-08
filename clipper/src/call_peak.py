c0c5974e1c2c096d45a8dc3fa0daf8b811fbde2f
Gabriel Pratt <gpratt@ucsd.edu>
2014-08-08 11:23:52 -0700

    call peak modified to use minimze scalar

--- clipper/src/call_peak.py
+++ clipper/src/call_peak.py
@@ -585,6 +585,7 @@ class SmoothingSpline(PeakGenerator):
                 best_smoothing_estimate = cur_smoothing_value
                 best_error = cur_error
 
+
         try:
             #fine optimization of smoothing parameter
             #low-temp optimize
