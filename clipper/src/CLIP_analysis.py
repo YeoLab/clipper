386d0375d4805a30a0cf1ac8ff2aa80c422ab2f7
gpratt <gpratt@gmail.com>
2014-10-24 16:08:31 -0700

    fixed bug in assign to regions where regions weren't assigned to bed files without strand.

--- clipper/src/CLIP_analysis.py
+++ clipper/src/CLIP_analysis.py
@@ -226,10 +226,14 @@ def assign_to_regions(tool, clusters, regions, assigned_dir=".", species="hg19",
     bed_dict['all']['real'] = pybedtools.BedTool("", from_string=True)
     
     offsets = get_offsets_bed12(tool)
-    if len(list(tool[0])) < 8:
+    if len(list(tool[0])) <= 5:
+        print "foo"
+        tool.sort().merge().saveas()
+    elif 6 <= len(list(tool[0])) < 8:
         tool = tool.sort().merge(s=True, c="4,5,6", o="collapse,collapse,collapse", stream=True).each(fix_strand).saveas()
     else:
         tool = tool.sort().merge(s=True, c="4,5,6,7,8", o="collapse,collapse,collapse,min,min", stream=True).each(fix_strand).saveas()
+
     remaining_clusters = adjust_offsets(tool, offsets)
     
     print "There are a total %d clusters I'll examine" % (len(tool))
