7910f07722c2ff2f65a149efaa9b0d77d72bede6
gpratt <gpratt@gmail.com>
2018-02-06 10:34:15 -0800

    updated kmerdiff to work with jellyfish (2.0) instead of compseq, it can now handle arbitray k values

--- clipper/src/kmerdiff.py
+++ clipper/src/kmerdiff.py
@@ -38,6 +38,8 @@ import sys
 import subprocess
 from collections import namedtuple
 from math import sqrt
+import pandas as pd
+
 
 class Motif(namedtuple('Motif', ['freq1', 'freq2', 'delta'])):
     
@@ -47,103 +49,98 @@ class Motif(namedtuple('Motif', ['freq1', 'freq2', 'delta'])):
     
     """
 
-def kmer_diff(file1, file2, k):
-    
-    """
-    
-    ASSUMES EMBOSS IS INSTALLED AND IN YOUR PATH
-    (Need to include some checks for if this is not the case)
-    
-    file1 is a file to find words from
-    file2 is another file to find words from
-    k is the size of words to search for
-    
-    """
-    
-    
-    
-    if not os.path.exists(file1):
-        raise IOError(file1 + " does not exist")
-    if not os.path.exists(file2):
-        raise IOError(file2 + " does not exist")
-    
-    #call compseq
+
+def run_jellyfish(fn, out_file, k):
     with open(os.devnull, 'w') as fnull:
+        db_file = "{}.{}.jellyfish".format(fn, k)
+        jellyfish_db = ['jellyfish', 'count',
+                        '-m', str(k),
+                        '-s', '50M',
+                        fn,
+                        '-o', db_file]
+        jellyfish_dump = ['jellyfish', 'dump',
+                          '-c', '-t',
+                          '-o', out_file,
+                          db_file
+                            ]
         try:
-            subprocess.call(['compseq',
-                             '-word', str(k),
-                             '-sequence', file1,
-                             '-outfile', file1 + ".compseq"],
+            subprocess.call(jellyfish_db,
                                 shell=False,
                                 stdout=fnull,
                                 stderr=fnull
                             )
 
-            subprocess.call(['compseq',
-                             '-word', str(k),
-                             '-sequence', file2,
-                             '-outfile', file2 + ".compseq"],
+            subprocess.call(jellyfish_dump,
                                 shell=False,
                                 stdout=fnull,
-                                stderr=fnull
-                        )
-        except OSError as e:
-            print "compseq probably not installed, please install emboss package (http://emboss.sourceforge.net/download/) "
-            raise e
+                                stderr=fnull)
 
-    
-    n1, freq1 = parse_compseq(file1 + ".compseq")
-    n2, freq2 = parse_compseq(file2 + ".compseq")
-    
+        except OSError as e:
+            print "jellyfish probably not installed, please install"
+            raise e
+    df = pd.read_table(out_file,
+                       index_col=0,
+                       header=None,
+                       names=['kmer', 'count'])
+    return df
+
+
+def kmer_diff(file1, file2, k):
+
+    """
+
+    ASSUMES EMBOSS IS INSTALLED AND IN YOUR PATH
+    (Need to include some checks for if this is not the case)
+
+    file1 is a file to find words from
+    file2 is another file to find words from
+    k is the size of words to search for
+
+    """
+
+
+
+    if not os.path.exists(file1):
+        raise IOError(file1 + " does not exist")
+    if not os.path.exists(file2):
+        raise IOError(file2 + " does not exist")
+
+    freq1 = run_jellyfish(file1,
+             file1 + ".counts.txt",
+             k)
+    n1 = float(freq1.sum()['count'])
+
+    freq2 = run_jellyfish(file2,
+              file2 + ".random.counts.txt",
+             k)
+    n2 = float(freq2.sum()['count'])
+
+    freqs = pd.merge(freq1, freq2, left_index=True, right_index=True, how="outer", suffixes=["_freq1", "_freq2"])
+    freqs = freqs.fillna(0)
+
     results = {}
-    for key in freq1.keys():
-        
+    for key, row in freqs.iterrows():
+
         try:
-            g = (freq1[key] + freq2[key]) / (n1 + n2)
+            g = (row.count_freq1 + row.count_freq2) / (n1 + n2)
         except ZeroDivisionError:
             g = 0
-            
-        #delta is some sort of strange change measurement, not quite 
+
+        #delta is some sort of strange change measurement, not quite
         if g == 0:
             delta = 0
         else:
-            delta = ((freq1[key] / n1) - (freq2[key] / n2)) / sqrt((1/n1 + 1/n2) * g * (1-g))
-        
-        results[key] = Motif(freq1[key], freq2[key], delta)
-        
+            delta = ((row.count_freq1 / n1) - (row.count_freq2 / n2)) / sqrt((1/n1 + 1/n2) * g * (1-g))
+
+        results[key] = Motif(row.count_freq1, row.count_freq2, delta)
+
     return results, n1, n2
-
-def parse_compseq(file):
-    
-    """
-    
-    parses compseq file to return the total number of reported kmers
-
-    Returns a tuple
-    total - other: The total number of kmers counted and reported
-    result: A dict {kmer : count times kmer is observed}
-    consider refactoring to just store the results in a proper data structure with everything accessable
-
-    """
-  
-    fi = open(file)
-    
-    result = {}
-    for line in fi:
-        line_split = line.strip().split("\t")
-        if line.startswith("Total count"):
-            total = float(line_split[1])
-        elif line.startswith("Other"):
-            other = float(line_split[1])
-        elif not (line.startswith("#") or line.startswith("Word size") or line.startswith("\n")):
-            result[line_split[0]] = float(line_split[1])
-    return total - other, result
 
 if __name__ == "__main__":
     file1 = sys.argv[1]
     file2 = sys.argv[2]
     k = sys.argv[3]
-    results, n1,n2 =  kmer_diff(file1, file2, k)
+    results, n1, n2 = kmer_diff(file1, file2, k)
     for key in results.keys():
         result = results[key]
         print "%s\t%s\t%s\t%s" % (key.lower(), str(result.freq1), str(result.freq2), result.delta)
